b20351be9880f58ce70e018383f0b921388d7efd
Carryl Sorene <carrylsorene@gmail.com>
2015-10-05 11:04:57 +0100

    MongoDB setup

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 var port = process.env.PORT || 3000
-var mongoose = require('mongoose')
+var mongoose = require('mongoose');
+
+//database setup
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/sports-partner');
 
 app.get('/', function(req, res){
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
 
-app.listen(port)
+app.listen(port);
