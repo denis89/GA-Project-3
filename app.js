754f131937eef85754f7e211d109569d5252d2e3
Carryl Sorene <carrylsorene@gmail.com>
2015-10-05 15:09:17 +0100

    DB seeded with six dummy users

--- app.js
+++ app.js
@@ -8,20 +8,21 @@ var seeder = require('mongoose-seed');
 
 //database setup
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/sports-partner');
+// mongoose.connect('mongodb://localhost:27017/sports-partner');
 
-// seeder.connect('mongodb://localhost:27017/sports-partner', function() {
+var data = require('./userseed.js');
+seeder.connect('mongodb://localhost:27017/sports-partner', function() {
 //seeder code - all goes here?
   // Load Mongoose models 
-  // seeder.loadModels([
+  seeder.loadModels([
       './models/user.js',
-  // ]);
+  ]);
   // Clear specified collections 
-  // seeder.clearModels(['User'], function() {
+  seeder.clearModels(['User'], function() {
   // Callback to populate DB once collections have been cleared 
-  // seeder.populateModels(data);
-  // });
-// });
+  seeder.populateModels(data);
+  });
+});
 // end of seeder code
 
 // Routes
